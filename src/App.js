ec226f88d2168ec71d2fdf23e89c5d92b7a20d07
Cibaax <sebasfer@outlook.com>
2022-10-08 12:36:25 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Character from "./components/Character/Character";
 import Home from "./components/Home/Home"
 import Episodes from "./components/Episodes/Episodes";
 import Episode from "./components/Episode/Episode";
+import Music from "./components/Music/Music";
 
 
 
@@ -15,7 +16,7 @@ function App() {
           <Route path="/" element={<Home/>}></Route>
           <Route path="/characters" element={<Characters/>}></Route>
           <Route path="/character/:id" element={<Character/>}></Route>
-          <Route path="/music"></Route>
+          <Route path="/music"element={<Music/>}></Route>
           <Route path="/episodes" element={<Episodes/>}></Route>
           <Route path="/episode/:id" element={<Episode/>}></Route>
         </Routes>
